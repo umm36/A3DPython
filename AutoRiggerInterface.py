342644bd30aa83a7ab028d7d00f1dcf6dbaec666
Jeremy <ummmm36@gmail.com>
2020-07-23 11:35:28 +1200

    AddShoulderGroups

--- AutoRiggerInterface.py
+++ AutoRiggerInterface.py
@@ -24,7 +24,7 @@ def createLocators():
         base.group(em = True, name = "Loc_Master")
     
     root = base.spaceLocator(n = 'Loc_ROOT')
-    base.scale(10,10,10, root)
+    #base.scale(10,10,10, root)
     base.move(0, 95, 2, root)
     base.parent(root, "Loc_Master")
     
@@ -33,13 +33,32 @@ def createLocators():
 def createSpine():
     for i in range (0, base.intField(spineCount, query = True, value = True)):
         spine = base.spaceLocator(n = "Loc_SPINE_" + str(i))
-        base.scale (10,10,10, spine)
+        base.scale (1,1,1, spine)
         if i == 0:
             base.parent(spine, 'Loc_ROOT')
         else:
             base.parent(spine, 'Loc_SPINE_' + str(i-1))
-        base.move(0, 1.25 + (0.25 * i), 0, spine)
-       
+        base.move(0, 1.25 + (10.25 * i), 0, spine)
+    createArms(1)
+    createArms(-1)
+
+
+def createArms(side):
+    if side == 1: #left
+        if base.objExists("L_Arm_GRP"):
+            print "Nah"
+        else:
+            L_arm = base.group(em = True, name = "L_Arm_GRP")
+            base.parent(L_arm, "Loc_SPINE_" + str(base.intField(spineCount, query = True, value = True) - 1))
+            base.move(12.338 * side, 148.887, -1.486, L_arm)
+    else: #right
+        if base.objExists("R_Arm_GRP"):
+            print "Naaaah"
+        else:
+            R_arm = base.group(em = True, name = "R_Arm_GRP")
+            base.parent(R_arm, "Loc_SPINE_" + str(base.intField(spineCount, query = True, value = True) - 1))
+            base.move(12.338 * side, 148.887, -1.486, R_arm)
+
 def deleteLocators():
     nodes = base.ls("Loc_*")
     base.delete(nodes)
